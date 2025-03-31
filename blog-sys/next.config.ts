bf34fbcd269f658bb951f5038f1a5e3c106b6452
Arnold <149857208+PrimeBeyonder@users.noreply.github.com>
2025-03-31 19:55:08 +0630

    complete project set up

--- blog-sys/next.config.ts
+++ blog-sys/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcrypt'],
+  },
 };
 
 export default nextConfig;
