09cbbbf8574bc9d07a904767aecfb17eb6885f2b
Myat Kyaw Thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2025-10-30 17:24:16 +0630

    prefix profile images

---
+++ lib/constants/profile-images.ts
@@ -0,0 +1,12 @@
+// Available profile images
+export const AVAILABLE_PROFILE_IMAGES = [
+  "/images/profiles/profile_1.jpeg",
+  "/images/profiles/profile_2.jpeg",
+  "/images/profiles/profile_3.jpeg",
+  "/images/profiles/profile_4.jpeg",
+  "/images/profiles/profile_5.jpeg",
+  "/images/profiles/profile_6.jpeg",
+  "/images/profiles/profile_7.jpeg",
+  "/images/profiles/profile_8.jpeg",
+  "/images/profiles/profile_9.jpeg",
+]
